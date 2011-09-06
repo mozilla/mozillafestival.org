06d406550d7ee9c180e3e509a833c397fd825765
Ross Bruniges <rossbruniges@gmail.com>
2011-09-06 18:03:58 +0100

    Further alterations to the template to get the sidebar showing as expected and looking as designs

---
+++ wp-content/themes/mozilla-festival/tmpl-one-col-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: One column
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+		<div id="primary" class="one_col">
+			<div id="content" role="main">
+
+				<?php the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php comments_template( '', true ); ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
