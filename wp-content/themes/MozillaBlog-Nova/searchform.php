cb08df6a0ad8a98f4e6c77018b86794b13f25c82
Ross Bruniges <rossbruniges@gmail.com>
2011-09-08 17:26:25 +0100

    A little bit of a final spring clean

--- wp-content/themes/MozillaBlog-Nova/searchform.php
+++
@@ -1,5 +0,0 @@
-<form method="get" id="search" action="<?php bloginfo('url'); ?>">
-  <fieldset>
-    <p><input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="<?php _e('Search Blog','mozblog'); ?>"> <button type="submit">Search</button></p>
-  </fieldset>
-</form>
