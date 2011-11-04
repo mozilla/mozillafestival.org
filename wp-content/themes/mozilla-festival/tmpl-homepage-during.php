01b02d502dd2bbe08ff30f0d07d4616a4eb7f50a
Ross Bruniges <rossbruniges@gmail.com>
2011-11-04 10:49:20 +0000

    Fixing the homepage template

--- wp-content/themes/mozilla-festival/tmpl-homepage-during.php
+++ wp-content/themes/mozilla-festival/tmpl-homepage-during.php
@@ -18,12 +18,12 @@ get_header(); ?>
             <img id="info_graphic" src="<?php bloginfo('stylesheet_directory'); ?>/images/fest_info.png" alt="open web developers, journalists, media educators - we're getting together for 3 days to build real projects that will change media, the web and the world" />
 			<div id="content" role="main">
                 <section class="buzz">
-                    <h2 class="entry-title">Latest Festival news</h2>
+                    <h2 class="entry-title">What's happening</h2>
                     <div>
                     <iframe src="http://embed.scribblelive.com/Embed/v5.aspx?Id=32231&amp;ThemeId=3498" frameborder="0" height="300"></iframe>
                 </div>
                 <h3>Ravensborne Festival blogs</h3>
-                <p>The students at Ravensborne have been set the challenge to provide festival coverage. If you're looking for information on the following subjects then see what they've found:</p>
+                <p>The students at Ravensborne are providing festival coverage on the following topics:</p>
                 <ul>
                  <li>
                 <h3>Audio and Video</h3>
@@ -35,16 +35,16 @@ get_header(); ?>
                 </li>
                 <li>
                 <h3>Gaming</h3>
-                <a href="http://mozilla3.ravewebmedia.co.uk/">mozilla gaming</a>
+                <a href="http://mozilla3.ravewebmedia.co.uk/">Mozilla Gaming</a>
                 </li>
                <li>
                 <h3>Journalism</h3>
-                <a href="http://mozilla4.ravewebmedia.co.uk/">jour<em>new</em>ism</a>
+                <a href="http://mozilla4.ravewebmedia.co.uk/">Jour<em>new</em>ism</a>
                 </li>
                 </ul>
                 </section>
                 <section>
-                    <h2 class="entry-title">From the blog</h2>
+                    <h2 class="entry-title">More coverage</h2>
                     <ol class="grid">
                     <?php
                         query_posts('posts_per_page=6&category_name=uncategorized,mozfest,festival-blog');
@@ -76,13 +76,13 @@ get_header(); ?>
                 </section>
                 <section>
                     <h2 class="entry-title">Who's here?</h2>
-                    <p>If you're wondering who else is here then take a look at our <a href="http://lanyrd.com/2011/mozilla-festival/">Mozilla Festival Lanyrd page</a> and <a href="http://lanyrd.com/2011/mozilla-festival/attendees/">attendee directory</a> and of course if you've not added yourself please do so in order for people to know that you're here!</p>
+                    <p>Take a look at our  <a href="http://lanyrd.com/2011/mozilla-festival/">our Lanyrd page</a> or <a href="http://lanyrd.com/2011/mozilla-festival/attendees/">participant directory</a> . And please add yourself if you haven't already.</p>
                     <footer>
                         <?php
                             $category_ID = get_cat_ID('people');
                             $category_url = get_category_link($category_ID);
                         ?>
-                        <a class="register" href="/whos-coming/">See our key-note speakers and learning lab faciliators</a>
+                        <a class="register" href="/whos-coming/">See speakers and facilitators</a>
                     </footer>
                 </section>
                 <section class="partnerships">
@@ -122,29 +122,8 @@ get_header(); ?>
                     <a href="<?php echo get_the_content(); ?>"><?php the_post_thumbnail(); ?></a>
                     </li>
                     <?php endwhile; ?>
-                    </ul>
-                     <h3 class="partner-head">Human API partners</h3>
-                    <ul class="partners human">
-                    <?php
-                        query_posts('posts_per_page=-1&category_name=Partners&orderby=rand&tag=human-api');
-                        while (have_posts()) : the_post()
-                    ?>
-                    <li>
-                    <a href="<?php echo get_the_content(); ?>"><?php the_post_thumbnail(); ?></a>
-                    </li>
-                    <?php endwhile; ?>
-                    </ul>
-                     <h3 class="partner-head">Content partners</h3>
-                    <ul class="partners content">
-                    <?php
-                        query_posts('posts_per_page=-1&category_name=Partners&orderby=rand&tag=content-partners');
-                        while (have_posts()) : the_post()
-                    ?>
-                    <li>
-                    <a href="<?php echo get_the_content(); ?>"><?php the_post_thumbnail(); ?></a>
-                    </li>
-                    <?php endwhile; ?>
-                    </ul>
+                    </ul> 
+                  
                </section>
 			</div><!-- #content -->
 		</div><!-- #primary -->
