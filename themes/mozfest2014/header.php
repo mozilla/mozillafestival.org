6142d0f1cf9f00e4b450e5aec54eed399b070205
Mavis Ou <mmmavisss@gmail.com>
2014-05-20 02:20:38 -0700

    Responsive top nav

--- themes/mozfest2014/header.php
+++ themes/mozfest2014/header.php
@@ -24,26 +24,27 @@
           <button onclick="location.href='https://sendto.mozilla.org/page/signup/mozfest2014-save-the-date'">Sign up for updates</button>
         </div>
       </div>
-      <label id="mobile-nav-icon" for="show-navigation" title="Toggle Menu"></label>
+      <!-- <label id="mobile-nav-icon" for="show-navigation" title="Toggle Menu"></label> -->
+      <nav id="top-nav">
+        <div class="constrained">
+          <input type="checkbox" role="presentation" id="show-navigation">
+          <ul class="menu menu-count-<?php echo mf2012_menu_meta('header')->count + 1; ?>">
+            <?php if (esc_attr(get_theme_mod('mf2012_header_button_label')) != '') : ?>
+            <li class="register"><a href="<?php echo esc_attr( get_theme_mod( 'mf2012_header_button_target' ) ); ?>" class="button"><?php echo esc_attr( get_theme_mod( 'mf2012_header_button_label' ) ); ?></a></li>
+            <?php endif; ?>
+            <?php
+              if (has_nav_menu('header')) {
+                wp_nav_menu(array(
+                  'theme_location' => 'header',
+                  'container' => false,
+                  'items_wrap' => '%3$s',
+                  'depth' => 1,
+                ));
+              }
+            ?>
+          </ul>
+          <label id="mobile-nav-icon" for="show-navigation" title="Toggle Menu"></label>
+        </div>
+      </nav>
     </div>
-    <nav id="top-nav">
-      <div class="constrained">
-        <input type="checkbox" role="presentation" id="show-navigation">
-        <ul class="menu menu-count-<?php echo mf2012_menu_meta('header')->count + 1; ?>">
-          <?php if (esc_attr(get_theme_mod('mf2012_header_button_label')) != '') : ?>
-          <li class="register"><a href="<?php echo esc_attr( get_theme_mod( 'mf2012_header_button_target' ) ); ?>" class="button"><?php echo esc_attr( get_theme_mod( 'mf2012_header_button_label' ) ); ?></a></li>
-          <?php endif; ?>
-          <?php
-            if (has_nav_menu('header')) {
-              wp_nav_menu(array(
-                'theme_location' => 'header',
-                'container' => false,
-                'items_wrap' => '%3$s',
-                'depth' => 1,
-              ));
-            }
-          ?>
-        </ul>
-        <!-- <label id="mobile-nav-icon" for="show-navigation" title="Toggle Menu"></label> -->
-      </div>
-    </nav>
+    <div id="top-band"></div>
