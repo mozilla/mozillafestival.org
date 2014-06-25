0d40833fb8c94193214a14df71b33c098cf99ae0
Mavis Ou <mmmavisss@gmail.com>
2014-06-24 23:15:20 -0700

    Add 'Tracks & Sessions' Page Template

---
+++ themes/mozfest2014/page-tracks-and-sessions.php
@@ -0,0 +1,133 @@
+<?php
+/*
+ * Template Name: "Tracks & Sessions" Page
+ * Description:
+ */
+get_header();
+?>
+<div id="content">
+  <div class="constrained">
+
+    <!-- Tracks ===================== -->
+    <div>
+      <div class="header">
+        <h2>2014 Mozfest Tracks</h2>
+
+        <ul class="item-list">
+          <li>
+            <img class="alignleft" alt="picture of a book with gears inside: build and teach the web" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/build_teach_web.png" width="277" height="190" />
+            <h3>Build and Teach the Web</h3>
+            <h4>
+              Keep the web wild through hands-on making with innovative tools and teaching the web as a community.
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="three blocks on top of each other" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/physical_web.png" width="277" height="190" />
+            <h3>Open Web With Things</h3>
+            <h4>
+              Escape the limitations of your computer and build the web using sensors, circuits and good old paper and scissors.
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="game controller with a gear behind it" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/open_games.png" width="277" height="190" />
+            <h3>Web in Your Pocket</h3>
+            <h4>
+              Explore opportunities in the booming world of the open web on mobile. How can  we experiment & tinker to customize our own experience on our phones?
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="concentric circles, with the inner circle peeking out" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/whos_watching.png" width="277" height="190" />
+            <h3>Source Code for Journalism</h3>
+            <h4>
+              Design next-generation web solutions to solve critical issues facing news organizations and help journalism thrive on the open web.
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="a badge" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/skills_badges.png" width="277" height="190" />
+            <h3>Open Science and the Web</h3>
+            <h4>
+              Examine the potential of the open web to re-define how we experiment, analyze and share scientific knowledge.
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="city skyline drawn with arrows" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/connect_city.png" width="277" height="190" />
+            <h3>Art and Culture of the Web</h3>
+            <h4>
+              An exploration of the programs, practices and inspirations of open and networked digital art forms.
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="erlenmeyer flask" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/science_web.png" width="277" height="190" />
+            <h3>Open Badges Lab</h3>
+            <h4>
+              Challenge  the conventional system of recognizing skills and learning. Celebrate   achievements from Open Badge creators and issuers.
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="a globe next to an upward trending chart" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/open_data.png" width="277" height="190" />
+            <h3>Hive Learning Networks</h3>
+            <h4>
+              Join  this lab for people working on building local city learning networks  (Hives) and how they can better globally connect to share learning  experiences for youth and digital media.
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="a globe next to an upward trending chart" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/open_data.png" width="277" height="190" />
+            <h3>Hive Learning Networks</h3>
+            <h4>
+              Join this lab for people working on building local city learning networks (Hives) and how they can better globally connect to share learning  experiences for youth and digital media.
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="a globe next to an upward trending chart" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/open_data.png" width="277" height="190" />
+            <h3>Music Creators and the Open Web</h3>
+            <h4>
+              Play a role and explore what it takes to make music on the open web.
+            </h4>
+          </li>
+
+          <li>
+            <img class="alignleft" alt="a globe next to an upward trending chart" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/open_data.png" width="277" height="190" />
+            <h3>Privacy and Trust: Advocating for The Web We Want</h3>
+            <h4>
+              Use innovative tools to uncover how your data is exposed online, how to  build trust, and how to enable people to hold their leaders accountable to keep the free and open web.
+            </h4>
+          </li>
+
+           <li>
+            <img class="alignleft" alt="a globe next to an upward trending chart" src="//2014.mozillafestival.org/wp-content/uploads/2013/06/open_data.png" width="277" height="190" />
+            <h3>Open Data</h3>
+            <h4>
+              Uncover the data on the web and in our world that will help us better inform and organize our communities
+            </h4>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+
+
+    <!-- Sessions ===================== -->
+    <!-- <div>
+      <div class="header">
+        <h2>(eventually we'll list approved sessions here)</h2>
+      </div>
+    </div> -->
+
+
+  </div>
+</div>
+
+<?php
+
+get_footer();
+
+?>
