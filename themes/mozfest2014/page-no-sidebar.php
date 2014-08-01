eb023a60e973ea23ba672dac983e5a363459997d
William Duyck <fuzzyfox0@gmail.com>
2014-08-01 14:42:19 +0100

    Add sidebar-less template for pages

---
+++ themes/mozfest2014/page-no-sidebar.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Template Name: Default page w/o sidebar
+ * Description:
+ */
+
+get_header();
+
+?>
+<div id="content">
+	<div class="constrained">
+		<div id="content-inner">
+<?php
+
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+		$type = get_post_format();
+		if (empty($type)) $type = get_post_type();
+		get_template_part('content', $type);
+	}
+
+?>
+<div class="page-nav">
+    <div class="previous"><?php next_posts_link('&laquo; Older posts'); ?></div>
+    <div class="next"><?php previous_posts_link('Newer posts &raquo;'); ?></div>
+</div>
+<?php
+
+} else {
+	get_template_part('content', 'missing');
+}
+
+?>
+		</div>
+	</div>
+</div>
+<?php
+
+get_footer();
