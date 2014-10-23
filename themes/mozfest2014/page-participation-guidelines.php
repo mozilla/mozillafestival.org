b8774072892e2aa7ec4397b1703bdcd3686fa150
Mavis Ou <mmmavisss@gmail.com>
2014-10-23 12:06:00 +0100

    Add 'Participation Guidelines' page

---
+++ themes/mozfest2014/page-participation-guidelines.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * Template Name: "Participation Guidelines" Page
+ * Description:
+ */
+get_header();
+?>
+<div id="content">
+  <div class="constrained">
+
+    <div class="panel">
+      <div class="header">
+        <h2>Participation Guidelines</h2>
+      </div>
+      <div>
+        <p>
+          The Mozilla Festival respects Mozilla's <a title="community participation guidelines" href="https://www.mozilla.org/en-US/about/governance/policies/participation/">community participation guidelines</a>. These guidelines  cover our behaviour as participants, facilitators, space wranglers, staff, volunteers, vendors, and anyone else involved in making the MozFest possible.
+        </p>
+
+        <h4>Inclusion and Diversity</h4>
+        <p>
+          The Mozilla Project welcomes and encourages participation by everyone. It doesn’t matter how you identify yourself or how others perceive you: we welcome you.
+        </p>
+        <p>
+          We welcome contributions from everyone as long as they interact constructively with our community and everybody at the Mozilla Festival, including, but not limited to people of varied age, ability, culture, ethnicity, gender, gender-identity, language, race, sexual orientation, geographical location and religious & political views.
+        </p>
+        <p>
+          Mozilla-based activities should be inclusive and should support such diversity.
+        </p>
+
+        <h4>How we treat each other</h4>
+        <ul>
+          <li>Be respectful and welcoming</li>
+          <li>Try to understand different perspectives</li>
+          <li>Do not threaten violence</li>
+          <li>Empower others</li>
+          <li>Strive for excellence</li>
+          <li>Don’t expect to agree with every decision</li>
+        </ul>
+
+        <h4>Raising Issues at Mozfest</h4>
+        <p>
+          If you believe you’re experiencing practices at Mozfest which don’t meet the above policies, or if you feel you are being harrassed in any way, please immediately contact the Festival Director, Michelle Thorne. Your concerns will be treated confidentially and respectfully.
+        </p>
+
+        <p>
+          At the festival venue, contact the info desk and they will immediately find the Festival Director for you.
+        </p>
+        <ul>
+          <li>Email: michelle [at] mozillafoundation [.] org</li>
+          <li>Phone: +49 172 711 Nine Eight Zero Seven</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+
+get_footer();
+
+?>
