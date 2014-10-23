6bb4a232635f5baceb1e591b0c82c99172f660f5
Mavis Ou <mmmavisss@gmail.com>
2014-10-23 12:45:29 +0100

    Add 'Remote Participation' page

---
+++ themes/mozfest2014/page-remote-participation.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Template Name: "Remote Participation" Page
+ * Description:
+ */
+get_header();
+?>
+<div id="content">
+  <div class="constrained">
+
+    <div class="panel">
+      <div class="header">
+        <h2>Join in on the fun from anywhere in the world!</h2>
+      </div>
+      <div>
+        <p>If you can't make it to MozFest, don't worry. There are lots of great ways to still get involved.</p>
+
+        <h4>See what's going on</h4>
+        <ul>
+          <li>Chat along with the festival on Twitter using the <a href="https://twitter.com/search?q=mozfest">#MozFest hashtag</a>.</li>
+          <li>See updates and real-time pictures on the <a href="https://www.facebook.com/mozilla">Mozilla Facebook account</a> and on <a href="https://www.flickr.com/search?sort=relevance&text=%23MozFest">Flickr</a>, using the #MozFest hashtag.</li>
+          <li>Read what people are making and doing on our <a href="https://blog.webmaker.org/category/mozfest-event">blog</a>.</li>
+          <li>Watch live video from the keynote speeches and announcements, all broadcast from <a href="https://air.mozilla.org/">Air Mozilla's indoor blimp</a>.</li>
+        </ul>
+
+        <h4>Participate and organize</h4>
+        <ul>
+          <li>Participate from afar by completing the <a href="https://jess.makes.org/thimble/MTc4NjgzOTI5Ng==/mozfest-remotee-challenge">Mozfest Remotee Challenge</a>.</li>
+          <li>Remix this Thimble make to <a href="https://fuzzyfox.makes.org/thimble/i-believe-in-an-open-web-because">tell us why you believe in an open web</a>.</li>
+          <li>Organize your own “mini Mozfest” with your local community using these <a href="https://webmaker.org/event-guides">helpful guides</a>.</li>
+        </ul>
+
+        <h4>Celebrate </h4>
+        <ul>
+          <li>Share your experiences of making, learning and teaching the web on Twitter using the <a href="https://twitter.com/search?q=mozfest">#MozFest hashtag</a>.</li>
+          <li>After sharing and teaching with others, collect your <a href="https://webmaker.org/badges/skill-sharer">Web Literacy Skill Sharer badge</a>.</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+
+get_footer();
+
+?>
