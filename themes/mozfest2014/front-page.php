2e4dc5f7e3e7f10b354703e2380ef2ca547d7626
Mavis Ou <mmmavisss@gmail.com>
2014-10-28 14:07:51 +0000

    [ Bug 1077013 ] Post festival version (without graphic)

--- themes/mozfest2014/front-page.php
+++ themes/mozfest2014/front-page.php
@@ -7,22 +7,26 @@ get_header();
 <div id="carousel">
   <div class="constrained">
     <h2>MozFest 2014</h2>
-    <a class="button" href="http://schedule.mozillafestival.org/">Schedule</a>
+    <a class="button" href="https://blog.webmaker.org/mozfest-2014-we-made-this-together">See What We Made Together</a>
   </div>
 </div>
 
 <div id="content" class="splash-2014">
   <div class="constrained">
+    <div id="home-sidebar" class="section">
+      <div class="section">
+        <h3>Missed out on MozFest 2014?</h3>
+        <p>It's not too late to participate! Remix our <a href="https://jess.makes.org/thimble/LTgzMjI4MTY=/mozfest-remotee-challenge">Remotee Challenge</a>.</p>
+      </div>
+      <?php if ( is_active_sidebar( "home-primary" ) ) : ?>
+      <div id="blogs-list" class="section">
+        <?php dynamic_sidebar( "home-primary" ); ?>
+      </div>
+      <?php endif; ?>
+    </div>
     <div id="scribbleLive-wrapper">
       <div class="scrbbl-embed" data-src="/event/867043"></div>
       <script>(function(d, s, id) {var js,ijs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="//embed.scribblelive.com/widgets/embed.js";ijs.parentNode.insertBefore(js, ijs);}(document, 'script', 'scrbbl-js'));</script>
-    </div>
-    <div id="home-sidebar">
-      <?php if ( is_active_sidebar( "home-primary" ) ) : ?>
-        <div id="blogs-list">
-          <?php dynamic_sidebar( "home-primary" ); ?>
-        </div>
-      <?php endif; ?>
     </div>
   </div>
 </div>
