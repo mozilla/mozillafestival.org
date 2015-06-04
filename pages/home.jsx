dc6b6b6d70b703adf34727ee05e5a2309ccdd641
Scott <scott@localhost.localdomain>
2015-06-03 20:08:21 -0400

    embed video and play button hover state

--- pages/home.jsx
+++ pages/home.jsx
@@ -7,21 +7,46 @@ var Link = Router.Link;
 var ImageTag = require('../components/imagetag.jsx');
 
 var Home = React.createClass({
+  getInitialState: function() {
+    return {
+      playVideo: false
+    };
+  },
+  playVideo: function() {
+    this.setState({
+      playVideo: true
+    });
+  },
   render: function() {
+    var self = this;
     return (
       <div className="home-page">
         <Header logoImage="/assets/images/logo-mozilla-festival-white.svg"/>
         <HeroUnit image="/assets/images/home.jpg"
                   image2x="/assets/images/home.jpg">
-          <a className="video-link" href="https://www.youtube.com/watch?v=dYoiHhsdHCk">
-            <ImageTag src1x="/assets/images/img-play.svg"
-              alt="play button icon"/>
-          </a>
-          <h1>mozilla festival</h1>
-          <h2>november 6-8 in london</h2>
-          <div className="horizontal-rule"></div>
-          <div>Three days of building a truly global web&mdash;together.</div>
-          <div>Come with an idea, leave with a community.</div>
+          {function() {
+            if (self.state.playVideo) {
+              return (
+                <div className="iframe-container">
+                  <iframe width="640" height="390" src="https://www.youtube.com/embed/dYoiHhsdHCk?autoplay=1" frameborder="0" allowfullscreen></iframe>
+                </div>
+              );
+            } else {
+              return (
+                <div>
+                  <div onClick={self.playVideo} className="video-link">
+                    <ImageTag src1x="/assets/images/img-play.svg"
+                      alt="play button icon"/>
+                  </div>
+                  <h1>mozilla festival</h1>
+                  <h2>november 6-8 in london</h2>
+                  <div className="horizontal-rule"></div>
+                  <div>Three days of building a truly global web&mdash;together.</div>
+                  <div>Come with an idea, leave with a community.</div>
+                </div>
+              );
+            }
+          }()}
         </HeroUnit>
         <div className="centered content wide">
           <h1>Help Make the Web a Better Place</h1>
