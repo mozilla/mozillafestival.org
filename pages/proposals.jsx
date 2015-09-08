96331233076dca509e8ab38d72066d9f93594e84
Mavis Ou <mmmavisss@gmail.com>
2015-09-08 12:36:41 -0700

    tweaked header copy

--- pages/proposals.jsx
+++ pages/proposals.jsx
@@ -14,6 +14,7 @@ var Proposals = React.createClass({
         </HeroUnit>
         <div className="content">
           <div className="proposals-form">
+            <h1>Proposal deadline has passed.</h1>
             <h1>Thanks for your interest!</h1> 
             <p>The Call for Proposals is now closed. We will be reviewing all submissions over the next three weeks. If you submitted a proposal, we will contact you before the end of September to let you know. If you have any questions, please contact the team at <a href="mailto:festival@mozilla.org">festival@mozilla.org</a>.</p>
           </div>
