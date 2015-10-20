827524e0993e565a8f7429f81dcac139d811cdf1
Mavis Ou <mmmavisss@gmail.com>
2015-10-19 17:55:26 -0700

    Fixes #272 - Added Space icons to /sessions

--- pages/sessions.jsx
+++ pages/sessions.jsx
@@ -9,6 +9,7 @@ var ImageTag = require('../components/imagetag.jsx');
 var SpacesInfo = [
   {
     name: "Journalism",
+    iconPath: "/assets/images/space-icons/Journalism.svg",
     description: [ 
       "Journalism has an indelible impact on the Web. Code developed in newsrooms — like Django and Backbone — shape our experiences online; reporting amuses us, enrages us and leads to meaningful change in our communities; and millions of readers engage with online coverage daily.",
       "In the Journalism Space, we explore how journalism carried out both in and out of newsrooms can strengthen the open Web and improve people’s lives. Participants will learn how closely linked journalism and the Web truly are, and how they can contribute to the fourth estate online.",
@@ -26,6 +27,7 @@ var SpacesInfo = [
   },
   {
     name: "Science",
+    iconPath: "/assets/images/space-icons/Science.svg",
     description: [
       "Science and the Web both help us understand the world around us. In the Science Space, participants learn, tinker, explore and hack at the intersection of science and the Internet, and learn how the Web can advance scientific work.",
       "We cover topics like citizen science, open data, open mapping and open research, all the while learning news skills and tools. We also collaborate on each other’s projects and empower one another: our Open Research Accelerator provides one-on-one mentorship.",
@@ -44,6 +46,7 @@ var SpacesInfo = [
   },
   {
     name: "Digital Citizenship",
+    iconPath: "/assets/images/space-icons/Digital_Citizenship.svg",
     description: [
       "We believe everyone should be able to read, write and participate on the Web. The Digital Citizenship Space is all about unpacking how we achieve this.",
       "Limitations to digital citizenship are many: access, lack of privacy, marginalization, and socio-cultural beliefs that bleed into online life. Join a collection of policy experts, legal practitioners, activists and technologists from around the globe eager to solve these problems and increase online participation. Together we’ll discuss campaigns, tools and other avenues that can make a difference.",
@@ -68,6 +71,7 @@ var SpacesInfo = [
   },
   {
     name: "Mozilla Learning Networks",
+    iconPath: "/assets/images/space-icons/Mozilla_Learning_Networks.svg",
     description: [
       "The key to empowering individuals online and cementing the Web as a public resource is web literacy. In the Mozilla Learning Networks Space, we explore how to best teach and spread web literacy.",
       "We will convene and empower leaders from diverse backgrounds who are eager to teach the Web in their communities. Help us achieve the spread, scale and adoption of web literacy networks and programs around the globe.",
@@ -84,7 +88,8 @@ var SpacesInfo = [
     ]
   },
   {
-    name: "Global Villages",
+    name: "Global Village",
+    iconPath: "/assets/images/space-icons/Global_Village.svg",
     description: [
       "What is the Global Village? A collection of self-contained but interconnected places from around the world where participants at Mozfest meet, learn and tinker with tomorrow’s places. The Global Village cultivates leading practitioners to build, teach and advocate for an Internet of things that empowers its users.",
       "Turn off your screen. Close your book. End that meeting. Pick up a sketchpad, a pair of scissors, a hot-glue gun, some parcel tape and come cry “If We Build It They Will Come.” This is a springboard for tomorrow and welcoming place for those inclusive citizens and communities.",
@@ -103,6 +108,7 @@ var SpacesInfo = [
   },
   {
     name: "Participation",
+    iconPath: "/assets/images/space-icons/Participation.svg",
     description: [
       "Participation is at the heart of Mozilla’s approach to learning. But participation doesn’t just happen — it’s built through great design and great leadership.",
       "The Participation Space will be a mixture of workshops, hack sessions, open challenges and product tinkering aimed at building participation and developing leaders who can inspire others. Participants will cover personal leadership exploration, skill development and community building.",
@@ -119,6 +125,7 @@ var SpacesInfo = [
   },
   {
     name: "Youth Zone",
+    iconPath: "/assets/images/space-icons/Youth_Zone.svg",
     description: [
       "There’s sometimes a fundamental clash between youth and adults on the Web. Young people discover platforms and activities that are important and meaningful to them — but adults disagree, wishing youngsters would invest their time elsewhere. In the Youth Zone Space, we’ll explore and aim to reconcile this conflict.",
       "Young people and adults will work in tandem across three pathways and 30 sessions. Together, we’ll explore new technology and how it’s relevant to all participants. We’ll also build things together, and learn about fulfilling careers based in new media.",
@@ -135,6 +142,7 @@ var SpacesInfo = [
   },
   {
     name: "Localization",
+    iconPath: "/assets/images/space-icons/Localization.svg",
     description: [
       "All languages and cultures are uniquely different and valuable — and they should be treated this way on the Web, too. In order for the Web to empower everyone, it must be adapted to diverse communities and peoples. The next billion people to come online shouldn’t find an English-only Web.",
       "In the Localization Space, participants learn language localization skills and nuances, how to include diversity in products, and how to bring this philosophy back to their local communities.",
