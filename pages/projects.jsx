620287f56c3f174abb1c22b2a044293b863b88d2
Alan Mooiman <amooiman@gmail.com>
2016-11-07 12:02:18 -0500

    Add some context to projects, massage data
    Adjusted projects data so sizing of cards is more similar

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -3,20 +3,26 @@ var Header = require(`../components/header.jsx`);
 var Footer = require(`../components/footer.jsx`);
 var ProjectCard = require(`../components/project-card.jsx`);
 var projectData = require(`../mozfest-projects.json`);
+var HeroUnit = require('../components/hero-unit.jsx');
 
 var ProjectPage = React.createClass({
   render: function() {
     return (
       <div className="projects-page">
         <Header/>
+        <HeroUnit image="/assets/images/hero/projects.jpg"
+                image2x="/assets/images/hero/projects.jpg">
+        <h1>MozFest Projects</h1>
+        </HeroUnit>
         <div className="content wide">
-            <div className="p-y-3" name="Production" slug="production">
-              <h1>Mozfest Projects</h1>
+            <p>MozFest attracts and generates dozens of projects that thrive long after the festival weekend. See the examples below and find a catalyst for collaboration with our vibrant network.</p>
+            <div className="p-b-3" name="Production" slug="production">
               <div className="horizontal-rule"></div>
                 <div className="row">
                   {projectData.map((project, index)=>{ return <ProjectCard key={index} {...project } />; } )}
                 </div>
             </div>
+            <p>See more projects & assets from our network on <a href="http://mzl.la/pulse">Pulse</a>.</p>
         </div>
         <Footer/>
       </div>
