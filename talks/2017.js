1656d9de12a3d4e739d7bcadbacfa664702b2e86
ScottDowne <scott.downe@gmail.com>
2018-02-28 13:23:40 -0500

    Removed 2018 speakers which should have been in 2017, but left them commented out until we have videos.

--- talks/2017.js
+++ talks/2017.js
@@ -121,6 +121,19 @@ module.exports = [
       }
     ]
   },
+  /*{
+    name: ``,
+    videoLink: ``,
+    transcriptLink: ``,
+    speakers: [
+      {
+        name: `Katherine Maher `,
+        twitter: `@krmaher`,
+        pic: `/assets/images/speakers/2016/Maher.jpg`,
+        description: `Katherine Maher is the executive director of the Wikimedia Foundation, the nonprofit behind Wikipedia. Maher has deep experience in the non-profit and international sectors, with a particular focus on the intersection of technology and human rights, development, transparency and community building.`,
+      }
+    ]
+  },*/
   {
     name: ``,
     videoLink: `https://air.mozilla.org/mozfest-2017-sunday-dialogues-and-debates/#@21m19s`,
@@ -134,6 +147,20 @@ module.exports = [
       }
     ]
   },
+  /*{
+    name: ``,
+    videoLink: ``,
+    transcriptLink: ``,
+    speakers: [
+      {
+        name: `Ryan Merkley`,
+        twitter: `@ryanmerkley`,
+        pic: ``,
+        description: `Ryan Merkley is the CEO of Creative Commons, where he is building a vibrant, usable commons powered by collaboration and gratitude.
+Prior to Creative Commons, Ryan was Chief Operating Officer of Mozilla. He is an experienced campaigner and advocate for social causes, and has advised political campaigns on the local and national levels.`,
+      }
+    ]
+  },*/
   {
     name: ``,
     videoLink: `https://air.mozilla.org/mozfest-2017-saturday-dialogues-and-debates/#@1h31m16s`,
@@ -189,6 +216,19 @@ module.exports = [
       }
     ]
   },
+  /*{
+    name: ``,
+    videoLink: ``,
+    transcriptLink: ``,
+    speakers: [
+      {
+        name: `Mark Surman`,
+        twitter: `@msurman`,
+        pic: `/assets/images/speakers/2016/Surman.jpg`,
+        description: `Mark is Executive Director of the Mozilla Foundation, a global community that does everything from making Firefox to taking stands on issues like privacy and net neutrality. Mark’s main job is to build the movement side of Mozilla, rallying the citizens of the web, building alliances with likeminded organizations and leaders, and growing the open internet movement. Mark’s goal is nothing short of making the health and accessibility of the internet a mainstream issue around the world.`,
+      }
+    ]
+  },*/
   {
     name: ``,
     videoLink: `https://air.mozilla.org/mozfest-2017-sunday-dialogues-and-debates/#@52s`,
