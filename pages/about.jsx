66f13120b8e5e3cf4755ed4b5da3e5a0c82af706
ScottDowne <scott.downe@gmail.com>
2018-03-01 16:11:01 -0500

    About page changes.

--- pages/about.jsx
+++ pages/about.jsx
@@ -15,11 +15,11 @@ var TimeLineItem = React.createClass({
     return (
       <div className={className}>
         <div className="timeline-time-circle">{this.props.time}</div>
-        <div className="timeline-item-container w-100 d-flex align-items-center justify-content-center">
+        <div className="timeline-item-container w-100 d-flex justify-content-center">
           {function() {
             if (!self.props.flip) {
               return (
-                <div className="timeline-image-container">
+                <div className="timeline-image-container d-flex align-items-center justify-content-end">
                   <ImageTag src1x={self.props.image}
                     height={self.props.imageHeight} width={self.props.imageWidth}
                     alt={self.props.alt}/>
@@ -27,30 +27,32 @@ var TimeLineItem = React.createClass({
               );
             }
           }()}
-          <div className="timeline-content-container">
-            <div className="timeline-label">
-              {function() {
-                if (!self.props.flip) {
-                  return (
-                    <span className="label-line"></span>
-                  );
-                }
-              }()}
-              <span className="label">{this.props.label}</span>
-              {function() {
-                if (self.props.flip) {
-                  return (
-                    <span className="label-line"></span>
-                  );
-                }
-              }()}
+          <div className="timeline-content-container d-flex align-items-center">
+            <div>
+              <div className="timeline-label">
+                {function() {
+                  if (!self.props.flip) {
+                    return (
+                      <span className="label-line"></span>
+                    );
+                  }
+                }()}
+                <span className="label">{this.props.label}</span>
+                {function() {
+                  if (self.props.flip) {
+                    return (
+                      <span className="label-line"></span>
+                    );
+                  }
+                }()}
+              </div>
+              <div className="timeline-content">{this.props.children}</div>
             </div>
-            <div className="timeline-content">{this.props.children}</div>
           </div>
           {function() {
             if (self.props.flip) {
               return (
-                <div className="timeline-image-container">
+                <div className="timeline-image-container d-flex align-items-center">
                   <ImageTag src1x={self.props.image}
                     height={self.props.imageHeight} width={self.props.imageWidth}
                     alt={self.props.alt}/>
@@ -93,7 +95,7 @@ var About = React.createClass({
             </div>
             <div className="letter">
               <div className="half-content">
-                <p>This October marks our eighth MozFest. It’s also our most ambitious one yet &mdash; more than ever, the movement for a healthy Internet needs a place to convene, organize and act.</p>
+                <p>October 2017 marks our eighth MozFest. It’s also our most ambitious one yet &mdash; more than ever, the movement for a healthy Internet needs a place to convene, organize and act.</p>
                 <p>At its start, MozFest &mdash; then called Drumbeat and nestled in Barcelona’s Museum of Contemporary Art &mdash; featured a small band of hackers and makers.</p>
                 <p>Since that 2010 gathering, MozFest has grown significantly. In size, yes &mdash; but more importantly, in scope. In 2011, the festival turned its attention to digital media, welcoming journalists and newsroom coders into the fold. In 2013, we focused on web literacy, inviting educators from around the world to craft tools and curricula for teaching the web. And in 2016, we talked about digital inclusion: who isn’t unlocking opportunity online, why that is, and what we can do to fix it.</p>
                 <p>This is an evolution that mirrors the growth of the Internet health movement. Today, the concept of Internet health reaches far beyond the realm of open source code: it’s linked to civil liberties and public policy, free expression and inclusion. Discussions about the state of the web include engineers, but now also teachers, lawmakers, community organizers and artists.</p>
@@ -123,32 +125,40 @@ var About = React.createClass({
         </div>
         <div className="content centered wide">
           <div className="circles-container">
-            <CircleNumber number="1600+">
-              over 1.6k<br/>attendees
+            <CircleNumber number="1700+">
+              over 1.7k<br/>attendees
             </CircleNumber>
             <CircleNumber number="50+">
               participants from more<br/>than 50 countries
             </CircleNumber>
-            <CircleNumber number="325">
-              workshops<br/>& sessions
+            <CircleNumber number="312">
+              sessions<br/>& experiences
             </CircleNumber>
           </div>
-          <p>The eighth annual MozFest will be held in London, from Friday, Oct. 27 to Sunday, Oct. 29. MozFest is an annual celebration of the open Internet movement. It's where passionate technologists, educators, and makers come together to explore the future of the open Web.</p>
+          <p>The ninth annual MozFest will be held in London, from Friday, Oct. 26 to Sunday, Oct. 28. MozFest is an annual celebration of the open Internet movement. It's where passionate technologists, educators, and makers come together to explore the future of the open Web.</p>
         </div>
         <div className="timeline-container">
           <div className="starting-line"></div>
-            <TimeLineItem time="2016" label="inclusion" flip="true"
-              className="timeline-item-2016"
-              image="/assets/images/img-2016.svg"
-              alt="MozFest Flag"
-              imageHeight={230} imageWidth={220}
-            >
-              In 2016, we highlighted how the Internet movement’s boldest, most game-changing ideas can come from anyone, anywhere. We examined how issues like Web literacy, online privacy and encryption are relevant across the globe, and address challenges faced by people who don't yet feel they are welcome on the Web.
-            </TimeLineItem>
+          <TimeLineItem time="2017" label="Internet Health"
+            className="timeline-item-2017"
+            image="/assets/images/about-page-2017.jpg"
+            alt="2017 about image"
+            imageHeight={167} imageWidth={240}
+          >
+            MozFest 2017 was a deep exploration of the bright spots and threats to Internet health. Conversations, prototyping and art-making were framed around the ideas of decentralization, digital inclusion, online privacy and security, open innovation and Web literacy. Young people shone at the festival this year, with an 11-year old speaker on the main stage and youth-led sessions in every space.
+          </TimeLineItem>
+          <TimeLineItem time="2016" label="inclusion" flip="true"
+            className="timeline-item-2016"
+            image="/assets/images/img-2016.svg"
+            alt="MozFest Flag"
+            imageHeight={230} imageWidth={220}
+          >
+            In 2016, we highlighted how the Internet movement’s boldest, most game-changing ideas can come from anyone, anywhere. We examined how issues like Web literacy, online privacy and encryption are relevant across the globe, and address challenges faced by people who don't yet feel they are welcome on the Web.
+          </TimeLineItem>
           <TimeLineItem time="2015" label="training leaders"
             className="timeline-item-2015"
             image="/assets/images/about-1.png"
-            alt="2010 about image"
+            alt="2015 about image"
             imageHeight={343} imageWidth={534}
           >
             In 2015, we focused on leadership, advocacy and impact. We placed a particular emphasis on training tomorrow's leaders, empowering participants to make a positive difference on the Web, and working toward universal web literacy.
