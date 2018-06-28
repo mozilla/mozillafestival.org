0f511974cf25624b25ea136877008f6678fbbe51
Mavis Ou <mmmavis@users.noreply.github.com>
2018-06-27 17:25:05 -0700

    Related to #852, #922 - added wranglers bio & updated list of wranglers

--- team-bio/space-wranglers.js
+++ team-bio/space-wranglers.js
@@ -1,126 +1,284 @@
 let DECENTRALIZATION_WRANGLERS = [
   {
-    name: `Mark Boas`
+    name: `Mark Boas`,
+    twitter: `@maboa`,
+    bio: [
+      `Mark is an open web enthusiast and open-source developer based near Florence, Italy. A 2012 Knight-Mozilla OpenNews fellow, he enjoys working where web meets media. Co-founder of Trint, co-creator of Hyperaud.io, jPlayer, and Beastmaster at Bad Idea Factory, Mark is currently working with a number of nonprofit organisations.`
+    ]
   },
   {
-    name: `Kade Morton`
+    name: `Kade Morton`,
+    twitter: `@cypath`,
+    bio: [
+      `An information security practitioner, Kade is the founder of Aletheia, a community that’s building a decentralised, open-access publishing platform for scientific research and cultural preservation. He also runs Panopticon Project, an open database of open-source intelligence covering the capabilities of Advanced Persistent Threats, nation states, and corporations.`
+    ]
   },
   {
-    name: `Natasha Pillai`
+    name: `Natasha Pillai`,
+    twitter: ``,
+    bio: [
+      ``
+    ]
   },
   {
-    name: `Ross Schulman`
-  }
+    name: `Ross Schulman`,
+    twitter: `@RossSchulman`,
+    bio: [
+      `Ross Schulman is a senior counsel and senior policy technologist at New America’s Open Technology Institute, where he focuses on internet measurement, emerging technologies, surveillance, and decentralization. He is interested in where the open web meets decentralized networks to bring privacy, freedom, and shared experience to people around the world.`
+    ]
+  },
+  {
+    name: `Tendai Tirivanhu`,
+    twitter: ``,
+    bio: [
+      `Tendai is a young professional whose interests lie in financial literacy and technology education among women. She is passionate about bringing diversity to finance and technology spaces and cultural preservation.`
+    ]
+  },
 ];
 
 let DIGITAL_INCLUSION_WRANGLERS = [
   {
-    name: `Daniel R. Lucio`
+    name: `Daniel Lucio`,
+    twitter: `@danielrynelucio`,
+    bio: [
+      `Daniel is a community impact manager with Google Fiber based in Austin, Texas. From fighting border walls to registering voters, his work has focused on empowering underserved communities in the U.S. He is currently working on creating digital equity through digital inclusion and civic technology projects in Texas. `
+    ]
   },
   {
-    name: `Carolina Tejada`
+    name: `Carolina Tejada`,
+    twitter: `@cctalvarez`,
+    bio: [
+      `Caro is a learning strategist at Causa y Efecto, a Colombian start-up that improves the productivity of small and big organizations through new technologies and disruptive training techniques. Caro strives to make technology affordable and accessible for organizations of all sizes.`
+    ]
   },
   {
-    name: `Jon Tutcher`
+    name: `Jon Tutcher`,
+    twitter: ``,
+    bio: [
+      ``
+    ]
   },
   {
-    name: `Edoardo Viola`
+    name: `Edoardo Viola`,
+    twitter: `@edovio`,
+    bio: [
+      `Edoardo is an open-source enthusiast and community addict. He uses his powers to protect the open web and the people in his life against digital threats. He is a computer scientist devoted to helping people read, write, and participate on the web.`
+    ]
   }
 ];
 
 let OPENNESS_WRANGLERS = [
   {
-    name: `David Bild`
+    name: `David Bild`,
+    twitter: `@dbild`,
+    bio: [
+      `David is an educator in Chicago where he runs teen programs focused on urban ecology and environmental science. Teens in his program develop projects following open-science principles, use low-cost and DIY sampling methods, and create media-rich digital maps and other data visualizations that are posted on project websites.`
+    ]
   },
   {
-    name: `Mariano Blejman`
+    name: `Mariano Blejman`,
+    twitter: `@blejman`,
+    bio: [
+      `Mariano is an intrepreneur in media innovation across Latin America. He built media innovation acceleration projects, and in 2012 launched Media Party, the biggest media conference in Latin America. Now he run the digital strategy in a third media group in Argentina`
+    ]
   },
   {
-    name: `Dennis Ndegwa`
+    name: `Dennis Ndegwa`,
+    twitter: `@dendegwa`,
+    bio: [
+      `Dennis is a technology professional from Kenya. He has been involved in projects such as Kenya Open Data Initiative and the Digital Skills Observatory. He will be wrangling MozFest for the first time this year and hopes to bring a fresh new perspective. He enjoys meeting people and exchanging ideas.`
+    ]
   },
   {
-    name: `Princiya Sequeira`
+    name: `Princiya Sequeira`,
+    twitter: `@princi_ya`,
+    bio: [
+      `Princiya is a software engineer who does all things front-end on the web. JavaScript is her second love — food comes first. When not coding or cooking, she is an open-source, privacy, and diversity advocate. She is too obsessed these days to translate any tech jargon into meaningful cartoons.`
+    ]
   },
   {
-    name: `Marcos Vital`
+    name: `Marcos Vital`,
+    twitter: `@marcosvcvital`,
+    bio: [
+      `Marcos is a biologist, lecturer, and researcher from Brazil. He is passionate about teaching and science, and might try to convince you that biostatistics, R, dragonfly behaviour, or other random topics are most fascinating subjects ever. He is also a geek that loves books, video games, music, beer, and meeting people.`
+    ]
   },
   {
-    name: `Stephanie Wright`
+    name: `Stephanie Wright`,
+    twitter: `@shefw`,
+    bio: [
+      `Steph is a former (always?) librarian and open science nerd on Mozilla's Open Leadership team. As a self-proclaimed open data geek, she has a passion for the stories data can tell. If not in the Openness Space, you will probably find her at one of MozFest's many coffee carts throughout the festival — just look for the purple hair.`
+    ]
   },
   {
-    name: `Yo Yehudi`
+    name: `Yo Yehudi`,
+    twitter: `@yoyehudi`,
+    bio: [
+      `Yo is a research software engineer and community manager. By day she works at the University of Cambridge on an open-source biological data warehouse, and by night you'll find her passionately evangelising open-source code in science. She enjoys running, cycling, and sci-fi, and eats far too much avocado.  `
+    ]
   }
 ];
 
 let PRIVACY_AND_SECURITY_WRANGLERS = [
   {
-    name: `Jenn Beard`
+    name: `Jenn Beard`,
+    twitter: `@jenntbeard`,
+    bio: [
+      `A naturally curious person, Jenn enjoys learning, making craft cocktails, and traveling, particularly to island nations. According to her friends, she has yet to find a situation where she hasn’t made at least one friend. Jenn is a fairly open person, but bans connected personal assistant devices in her home.`
+    ]
   },
   {
-    name: `Teon Brooks`
+    name: `Teon Brooks`,
+    twitter: `@teon_io`,
+    bio: [
+      `Teon is a data scientist for the Test Pilot team at Mozilla. His background is in cognitive science and he is very interested in how we process and understand information in our real life and in our digital life.`
+    ]
   },
   {
-    name: `Mehan Jayasuriya`
+    name: `Mehan Jayasuriya`,
+    twitter: `@mehan_j`,
+    bio: [
+      `Mehan Jayasuriya is a program manager at Mozilla, where he oversees the NSF-WINS (Wireless Innovation for a Networked Society) Challenges and portions of the MOSS (Mozilla Open Source Support) program. He has previously worked for Tumblr, General Assembly, and NYU, and has written for The Guardian, Gizmodo, and Pitchfork.`
+    ]
   },
   {
-    name: `Mandy Rawlings`
+    name: `Mandy Rawlings`,
+    twitter: ``,
+    bio: [
+      `Mandy is the HR operations coordinator at Mozilla. She is a Toronto-based people person who is passionate about helping others succeed. Mandy believes that a healthy internet, and the safe and ethical use of data and insights, can make the world a better place to live.`
+    ]
   },
   {
-    name: `David Ross`
+    name: `David Ross`,
+    twitter: ``,
+    bio: [
+      `David is a highly-active member of Mozilla's UK volunteer community and a Mozilla Rep. Ocassionally found off-line hosting workshops, London MozCoffee, and a variety of other frequent events, and found online deeply embedded in decentralization, exploring ethical alternatives, metamodernism, activism, LGBTQIA rights, and building Fit Open — a people-powered outdoor fitness and endurance sports social enterprise. `
+    ]
   },
   {
-    name: `Vojtech Sedlak`
+    name: `Vojtech Sedlak`,
+    twitter: `@vojtechsedlak`,
+    bio: [
+      `Vojtech is a digital analyst. Originally from the Czech Republic, Vojtech works at the intersection of programming, analytics, and storytelling. He enjoys searching for innovative ways of analyzing data and communicating insights and is a passionate advocate of the open data and open analytics movements.`
+    ]
   }
 ];
 
 let WEB_LITERACY_WRANGLERS = [
   {
-    name: `Juma Baldeh`
+    name: `Juma Baldeh`,
+    twitter: `@TechAngelGirl`,
+    bio: [
+      `Juma preceded the Mozilla club HackathonGirls, which teaches web literacy to young girls. She is the country manager for Kubo, an open school platform in Gambia, and is also a Firefox champion. Juma envisions a future where there is a strong, equitable role on the web for the next generation of women.`
+    ]
   },
   {
-    name: `Geraldo Barros`
+    name: `Geraldo Barros`,
+    twitter: `@geraldobarros_`,
+    bio: [
+      `Geraldo is a front-end developer, facilitator, and educator with a history of working in technology and social innovation. He is passionate about teaching digital inclusion and web literacy to local communities and is among Mozilla's 2017 Network50 awardees. `
+    ]
   },
   {
-    name: `Winnie Makokha`
+    name: `Winnie Makokha`,
+    twitter: `@IamProvisional`,
+    bio: [
+      `Winnie’s background spans the fields of environmental and occupational health, safety, research, and use of technology for advocacy. She was a researcher in the Digital Skills Observatory, a Mozilla project in Kenya. She is currently a safety supervisor at Mpesa Foundation Academy, Kenya. She brings conversations about feminism to the web.`
+    ]
   },
   {
-    name: `Priyanka Nag`
+    name: `Priyanka Nag`,
+    twitter: `@priynag`,
+    bio: [
+      `Priyanka is a technical writer at Red Hat. Though her primary skill is supposed to be writing, she is mostly a total chatter box. She is an open-source enthusiast who likes to hang out with geeks, drink a lot of coffee, and spend way too much of her time in front of the black terminal.`
+    ]
   },
   {
-    name: `Ryan Warsaw`
+    name: `Berhan Taye`,
+    twitter: `@btayeg`,
+    bio: [
+      `Berhan Taye is a campaigner at Access Now. She leads the #KeepItOn campaign, which fights internet shutdowns across the world. She is an activist and a researcher who works at the intersection of social justice and tech. Berhan was a Ford-Mozilla Open Web Fellow, and this is her second MozFest.`
+    ]
+  },
+  {
+    name: `Ryan Warsaw`,
+    twitter: `@ryanwse`,
+    bio: [
+      `Ryan is a software engineer at Enki and core contributor at Mozilla. He is oriented around the learning space, and in his free time contributes to Mozilla's previous and current learning products such as Webmaker, Thimble, and the Digital Skills Observatory initative.`
+    ]
   }
 ];
 
 let YOUTH_ZONE_WRANGLERS = [
   {
-    name: `Dervla O Brien`
+    name: `Dervla O'Brien`,
+    twitter: `@dervlaobrien`,
+    bio: [
+      `A student of computer science at Cardiff University, Dervla dreams of a world where the gap between the arts and sciences has been bridged. Her experience in youth work and with the Corrymeela community has led her to finding and building intentional, inclusive communities online.`
+    ]
   },
   {
-    name: `Tina Gotschi`
+    name: `Tina Gotschi`,
+    twitter: ``,
+    bio: [
+      ``
+    ]
   },
   {
-    name: `Emrys Green`
+    name: `Emrys Green`,
+    twitter: `@emrysgreen`,
+    bio: [
+      `Emrys is a digital and arts project manager working on a range of projects in the UK.  Emrys works for Upstart Projects & Cloud Artisans and has a background in participation policy and theatre.`
+    ]
   },
   {
-    name: `Alan McCullagh`
+    name: `Alan McCullagh`,
+    twitter: `@tyrower`,
+    bio: [
+      `Alan is Irish and now French, too. He fell into computers aged five with LOGO, Turtle and BBC Micros. During 14 years in Vaucluse, he's spent time in language and tech support. He enjoys working on digital education, getting paid to play with Robots, Raspberry Pi and Scratch, and transmitting a passion for geekiness.`
+    ]
   },
   {
-    name: `Andrew Mulholland`
+    name: `Andrew Mulholland`,
+    twitter: `@gbaman1`,
+    bio: [
+      `A computer science graduate, Andrew is passionate about getting kids excited and involved in computer science and digital making. In his free time, he can be found working with schools and running events including the Northern Ireland Raspberry Jam.`
+    ]
   },
   {
-    name: `Marie-jubilee Muntunkaye`
+    name: `Marie-jubilee Muntunkaye`,
+    twitter: `@MarieJ_ADA`,
+    bio: [
+      `MJ is a student at Ada, the National College For Digital Skills who is currently studying computer science and graphics. MJ is passionate about art, technology, and women in tech.`
+    ]
   },
   {
-    name: `Simone Ram`
+    name: `Simone Ram`,
+    twitter: `@Adacollege`,
+    bio: [
+      `Simone Ram is a student learning computer science. He is a very friendly and approachable person eager to help you overcome any problem. Simone's interests are in technology, biology, and the arts.  `
+    ]
   },
   {
-    name: `Chad Sansing`
+    name: `Chad Sansing`,
+    twitter: `@chadsansing`,
+    bio: [
+      `Chad is a curriculum manager at Mozilla working on open leadership and events. Before joining Mozilla, Chad spent 14 years in the classroom teaching English, social studies, and technology.  This is his sixth MozFest.`
+    ]
   },
   {
-    name: `Victor Sansing`
+    name: `Victor Sansing`,
+    twitter: ``,
+    bio: [
+      `Victor is a sophomore in high school. He brings his curiosity and ideas about how to engage young people with MozFest to the team.`
+    ]
   },
   {
-    name: `Joseph Thomas`
+    name: `Joseph Thomas`,
+    twitter: ``,
+    bio: [
+      ``
+    ]
   }
 ];
 
