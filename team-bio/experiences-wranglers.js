73c8b37c63376cd9ba50ad21378985f14242fedb
Mavis Ou <mmmavis@users.noreply.github.com>
2018-06-28 10:28:20 -0700

    Added wranglers bio & updated list of wranglers (#975)
    * Related to #852, #922 - added wranglers bio & updated list of wranglers

--- team-bio/experiences-wranglers.js
+++ team-bio/experiences-wranglers.js
@@ -1,12 +1,17 @@
 let QUEERING_MOZFEST = [
   {
-    name: `Emma Humphries`
+    name: `Aurelia Moser`,
+    twitter: `@auremoser`,
+    bio: [
+      `Aure is a curious coder at Mozilla, working on internet health-focused fellowships. She also co-leads the NYC chapter of Girl Develop It, teaches web-programming at Parsons-J+D, SVA-DSI, and NYU-ITP, plus she gives talks and writes books on geospatial tech.`
+    ]
   },
   {
-    name: `Aurelia Moser`
-  },
-  {
-    name: `Stephanie Ouillon`
+    name: `Stephanie Ouillon`,
+    twitter: ``,
+    bio: [
+      `Stéphanie is a security engineer at Mozilla working on Firefox. She loves exploring how tech and politics intersect.`
+    ]
   }
 ];
 
