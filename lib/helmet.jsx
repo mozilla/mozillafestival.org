b463eb3c505c2de09760e8714c29c31cdcabe404
Mavis Ou <mmmavis@users.noreply.github.com>
2018-07-26 19:16:17 -0700

    Related to #918 - updated site's description & fixed some <meta> not being correctly set issues (#1015)

--- lib/helmet.jsx
+++ lib/helmet.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 export default (description) => {
-  const title = `Mozilla Festival`;
+  const TITLE = `Mozilla Festival (MozFest)`;
 
   if (!description) {
-    description = `Three days of building a truly global web together. Come with an idea, leave with a community.`;
+    description = `Mozilla Festival (MozFest) brings the world together to leave the internet better than we found it. Mozilla, the non-profit behind the Firefox browser, invites you to our annual interactive festival in London, England. Come shape our best future online.`;
   }
 
-  return <Helmet defaultTitle={title}>
-    <title>{title}</title>
+  return <Helmet defaultTitle={TITLE}>
+    <title>{TITLE}</title>
     <meta name="description" content={description} />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://mozillafestival.org" />
-    <meta property="og:title" content={title} />
+    <meta property="og:title" content={TITLE} />
     <meta property="og:description" content={description} />
     <meta property="og:image" content="https://mozillafestival.org/assets/images/site-thumbnail.jpg" />
   </Helmet>;
