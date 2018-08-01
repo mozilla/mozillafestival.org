289e3585c0e5eae9fac67da15fe3c38100188bc3
Mavis Ou <mmmavis@users.noreply.github.com>
2018-08-01 11:19:15 -0700

    remove unused imported module

--- pages/spaces.jsx
+++ pages/spaces.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Jumbotron from '../components/jumbotron.jsx';
 
 var SpaceProfile = require('../components/space-profile.jsx');
