cba1138f6946bce30a8c9d25999095a07a05d006
Mavis Ou <mmmavis@users.noreply.github.com>
2018-08-01 11:22:07 -0700

    Remove CFP CTA link on /spaces page (#1039)
    * Related to #1018 - remove CFP CTA link on /spaces page
    
    * remove unused imported module

--- pages/spaces.jsx
+++ pages/spaces.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Jumbotron from '../components/jumbotron.jsx';
 
 var SpaceProfile = require('../components/space-profile.jsx');
@@ -108,7 +107,6 @@ var SessionsPage = React.createClass({
           <div className="content centered wide">
             <h1>2018 Spaces</h1>
             <p>Spaces are physical and thematic learning hubs based around a broad topic, like web literacy or digital inclusion. A Space is made up of sessions, which are hands-on, educational gatherings based around a specific topic, like “Using Crafts to Teach Localization Processes.” Sessions generally run 30-90 minutes.</p>
-            <div><Link to="/proposals" className="btn btn-arrow"><span>Propose a Session</span></Link></div>
             <div className="horizontal-rule"></div>
           </div>
         </div>
