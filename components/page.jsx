7067e8908223b56384dc9e534e8bf9586f5750a9
Mavis Ou <mmmavis@users.noreply.github.com>
2018-08-02 13:52:27 -0700

    Related to #1043 - removed remote.jsx and its related files

--- components/page.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var Page = React.createClass({
-  render: function() {
-    return <div className={this.props.name + "-page"}>{ this.props.children }</div>;
-  }
-});
-
-module.exports = Page;
