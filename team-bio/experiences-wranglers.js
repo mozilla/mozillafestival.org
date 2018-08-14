b8d9930c58cb24a6823f10eac348256b8225ac00
Mavis Ou <mmmavis@users.noreply.github.com>
2018-08-14 14:43:18 -0700

    Related to #1022 - added Art + Data wranglers

--- team-bio/experiences-wranglers.js
+++ team-bio/experiences-wranglers.js
@@ -1,3 +1,29 @@
+const ART_AND_DATA = [
+  {
+    name: `Luca M. Damiani`,
+    pic: `/assets/images/team/wrangler/experience/art-data_luca-m-damiani_500x500.jpg`,
+    bio: [
+      `Luca is a media artist practicing internationally in the fields of the arts and design, technology, and visual culture. He’s a Lecturer in graphic and media design at University of the Arts London, and digital producer at Tate. Luca is always creating ongoing conversations about the role of the arts in society.`
+    ]
+  },
+  {
+    name: `Irini Papadimitriou`,
+    pic: `/assets/images/team/wrangler/experience/art-data_irini-papadimitriou_500x500.jpg`,
+    bio: [
+      `Irini is a curator and producer working at the forefront of digital culture. As digital programmes manager at the V&A, Irini is responsible for the annual Digital Design Weekend. Irini is also head of new media arts development at Watermans, and co-founder of Maker Assembly, a critical gathering about maker cultures.`
+    ]
+  },
+  {
+    name: `Angela Plohman`,
+    twitter: `@angelaplohman`,
+    pic: `/assets/images/team/wrangler/experience/art-data_angela-plohman_500x500.jpg`,
+    bio: [
+      `Angela is the executive vice president of the Mozilla Foundation. She has spent close to two decades playing key leadership roles in the fields of art, culture, and open-source technology. Prior to Mozilla, she was director of Baltan Laboratories, a laboratory for art and technology based in Eindhoven (NL).`
+    ]
+  }
+];
+
+
 const QUEERING_MOZFEST = [
   {
     name: `Aurelia Moser`,
@@ -18,6 +44,6 @@ const QUEERING_MOZFEST = [
 ];
 
 module.exports = {
-  "Art + Data": [],
+  "Art + Data": ART_AND_DATA,
   "Queering MozFest": QUEERING_MOZFEST
 };
