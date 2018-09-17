8f5c5e34b0d57f96784f2dc7c37fde7e4bcc06f2
Mavis Ou <mmmavis@users.noreply.github.com>
2018-09-17 16:11:52 -0700

    Related to #1093 - update D&D section on homepage

--- components/speakers-promo.jsx
+++ components/speakers-promo.jsx
@@ -5,67 +5,148 @@ import { Link } from 'react-router-dom';
 
 const FEATURED_SPEAKERS = [
   {
-    name: `Zeynep Tufekci`,
-    shortIntro: `professor and author`,
-    talkName: `The Algorithmic Spiral of Silence`,
-    link: `https://www.youtube.com/watch?v=pO-brBVRyN8&list=PLYiaJo7rYNXLQSEAa2RdyyiS28Ke2Rl60&index=12`,
-    pic: `/assets/images/speakers/2016/Tufekci.jpg`
+    name: `Amba Kak`,
+    shortIntro: ``,
+    twitter: `@ambaonadventure`,
+    talkName: `Talk: Data and Digital Rights in Asia`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Amba_Kak.jpg`
   },
   {
-    name: `Ashley Black`,
-    shortIntro: `comedienne`,
-    talkName: `A Pep Talk for Nerds`,
-    link: `https://www.youtube.com/watch?v=MFOdGSKSWDY&t=0s&list=PLYiaJo7rYNXInD71OAShY4xfl6fhskoUV&index=4`,
-    pic: `/assets/images/speakers/2017/AshleyBlack.jpeg`
+    name: `Zara Rahman`,
+    shortIntro: ``,
+    twitter: `@zararah`,
+    talkName: `Panel: The Data Balancing Act`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Zara_Rahman.jpg`
   },
   {
-    name: `Gisela Pérez de Acha`,
-    shortIntro: `lawyer and activist`,
-    talkName: `How to Hack an Earthquake`,
-    link: `https://www.youtube.com/watch?v=atYKpi_74Ak&t=0s&list=PLYiaJo7rYNXInD71OAShY4xfl6fhskoUV&index=2`,
-    pic: `/assets/images/speakers/2017/GiselaPerezdeAcha.jpeg`
+    name: `Malavika Jayaram`,
+    shortIntro: ``,
+    twitter: `@maljayaram`,
+    talkName: `Panel: The Data Balancing Act`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Malavika_Jayaram.jpg`
   },
   {
-    name: `Audrey Tang`,
-    shortIntro: `Taiwan’s digital minister`,
-    talkName: `Stories from the Future of Democracy`,
-    link: `https://www.youtube.com/watch?v=mzjHu2izFn8&t=0s&list=PLYiaJo7rYNXInD71OAShY4xfl6fhskoUV&index=9`,
-    pic: `/assets/images/speakers/2017/AudreyTang.jpeg`
+    name: `Renee DiResta`,
+    shortIntro: ``,
+    twitter: `@noUpside`,
+    talkName: `Talk: Flaws in the Data-driven Digital Economy`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Renee_DiResta.jpg`
   },
   {
-    name: `Eliot Higgins`,
-    shortIntro: `Bellingcat founder`,
-    talkName: `Open Source and Flight MH17`,
-    link: `https://www.youtube.com/watch?v=iFx6eJF94Ew&index=2&list=PLYiaJo7rYNXLQSEAa2RdyyiS28Ke2Rl60`,
-    pic: `/assets/images/speakers/2016/Higgins.jpg`
+    name: `Julie Owono`,
+    shortIntro: ``,
+    twitter: `@JulieOwono`,
+    talkName: `Talk: Privacy in Sub-Saharan Africa`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Julie_Owono.jpg`
   },
   {
-    name: `Julia Angwin`,
-    shortIntro: `investigative journalist`,
-    talkName: `What Algorithms Taught Me About Forgiveness`,
-    link: `https://www.youtube.com/watch?v=Yei7SS8nEqw&t=0s&list=PLYiaJo7rYNXInD71OAShY4xfl6fhskoUV&index=12`,
-    pic: `/assets/images/speakers/2017/JuliaAngwin.jpeg`
+    name: `Esra’a Al-Shafei`,
+    shortIntro: ``,
+    twitter: `@ealshafei`,
+    talkName: `Panel: Data in Oppressive Regimes`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Esra'a_Al_Shafei.png`
   },
   {
-    name: `Emily May`,
-    shortIntro: `Hollaback! co-founder`,
-    talkName: `Being Human in the Dark Days of the Internet`,
-    link: `https://www.youtube.com/watch?v=zzJ0KAvpdVw&t=0s&list=PLYiaJo7rYNXInD71OAShY4xfl6fhskoUV&index=8`,
-    pic: `/assets/images/speakers/2017/EmilyMay.jpeg`
+    name: `Ramy Raoof`,
+    shortIntro: ``,
+    twitter: `@RamyRaoof`,
+    talkName: `Panel: Data in Oppressive Regimes`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Ramy_Raoof.jpg`
   },
   {
-    name: `Matt Mitchell`,
-    shortIntro: `Cryptoharlem founder`,
-    talkName: `It Takes a Hacker to Catch a Hacker`,
-    link: `https://www.youtube.com/watch?v=_mMhDA4z4BA&t=0s&list=PLYiaJo7rYNXInD71OAShY4xfl6fhskoUV&index=10`,
-    pic: `/assets/images/speakers/2017/MattMitchell.jpeg`
+    name: `Mahsa Alimardani`,
+    shortIntro: ``,
+    twitter: `@maasalan`,
+    talkName: `Panel: Data in Oppressive Regimes`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Mahsa_Alimardani.jpg`
   },
   {
-    name: `Nanjira Sambuli`,
-    shortIntro: `activist`,
-    talkName: `Closing the Digital Gender Divide`,
-    link: `https://www.youtube.com/watch?v=sECDr9LJlMs&t=0s&list=PLYiaJo7rYNXInD71OAShY4xfl6fhskoUV&index=13`,
-    pic: `/assets/images/speakers/2017/NanjiraSambuli.jpeg`
+    name: `Camille Francois`,
+    shortIntro: ``,
+    twitter: `@camillefrancois`,
+    talkName: `Panel: AI’s Collateral Damage`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Camille_Francois.jpg`
+  },
+  {
+    name: `Guillaume Chaslot`,
+    shortIntro: ``,
+    twitter: `@gchaslot`,
+    talkName: `Panel: AI’s Collateral Damage`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Guillaume_Chaslot.png`
+  },
+  {
+    name: `Alondra Nelson`,
+    shortIntro: ``,
+    twitter: `@alondra`,
+    talkName: `Panel: AI’s Collateral Damage`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Alondra_Nelson.jpg`
+  },
+  {
+    name: `Clinton Watts`,
+    shortIntro: ``,
+    twitter: `@selectedwisdom`,
+    talkName: `Panel: AI’s Collateral Damage`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Clinton_Watts.jpg`
+  },
+  {
+    name: `Roya Mahboob`,
+    shortIntro: ``,
+    twitter: `@royamahboob`,
+    talkName: `Panel: Inclusion and Literacy As Remedies`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Roya_Mahboob.jpg`
+  },
+  {
+    name: `Nathalie Richards`,
+    shortIntro: ``,
+    twitter: `@nathrich`,
+    talkName: `Panel: Inclusion and Literacy As Remedies`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Nathalie_Richards.jpg`
+  },
+  {
+    name: `Nani Jansen Reventlow`,
+    shortIntro: ``,
+    twitter: `@interwebznani`,
+    talkName: `Talk: Data and Platforms in Europe`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Nani_Jansen.jpg`
+  },
+  {
+    name: `Maryant Fernandez`,
+    shortIntro: ``,
+    twitter: `@maryantfp`,
+    talkName: `Panel: Who Controls the Internet?`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Maryant_Fernandez.jpg`
+  },
+  {
+    name: `Chris Riley`,
+    shortIntro: ``,
+    twitter: `@MChrisRiley`,
+    talkName: `Panel: Who Controls the Internet?`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Chris_Riley.jpg`
+  },
+  {
+    name: `Soudeh Rad`,
+    shortIntro: ``,
+    twitter: `@soudehrad`,
+    talkName: `Talk: Privacy, Identity, and Gender Online`,
+    link: ``,
+    pic: `/assets/images/speakers/2018/Soudeh_Rad.jpg`
   }
 ];
 
@@ -78,19 +159,20 @@ var SpeakersPromo = React.createClass({
       <div className="speakers-promo">
         <div className="row mb-5">
           <div className="col">
-            <h1>Past 'Dialogues & Debates' Speakers</h1>
+            <h1>Dialogues & Debates</h1>
             <p>Each year, MozFest features talks from luminaries at the intersection of technology and society. We host hackers, journalists, activists, and others on our stage. Here are just a few:</p>
           </div>
         </div>
         <div className="row">
           {
             FEATURED_SPEAKERS.map(speaker => {
-              return <div className="col-md-4" key={speaker.name}>
+              return <div className="col-md-3" key={speaker.name}>
                 <SpeakerTinyCard {...speaker} />
               </div>;
             })
           }
         </div>
+        { /*
         <div className="row">
           <div className="col text-center">
             <Link to="/speakers" className="btn btn-arrow">
@@ -98,6 +180,7 @@ var SpeakersPromo = React.createClass({
             </Link>
           </div>
         </div>
+        */ }
       </div>
     );
   }
